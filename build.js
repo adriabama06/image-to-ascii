0fd0a887995090a5dd27fdd82913190e5ab3ad9d
adriabama06 <adriabama06@gmail.com>
2022-09-14 23:33:37 +0200

    add my custom build, if has G++ cmake will use it and has automatic multi thread, automatically set -j <cores>

---
+++ build.js
@@ -0,0 +1,29 @@
+const { execSync } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+const os = require('os');
+
+if(fs.existsSync("build")) {
+    fs.rmSync("build", { recursive: true });
+}
+
+fs.mkdirSync("build");
+
+var hasGcc = false;
+
+try {
+    var stdout = execSync(`${process.platform == 'win32' ? 'where' : 'whereis'} g++`, {stdio:[]});
+    hasGcc = !!stdout;
+} catch {}
+
+process.stdout.write(
+    execSync(`cd "${path.join(__dirname, "build")}" && cmake ${hasGcc ? '-G "MinGW Makefiles" ' : ''}.. && cmake --build . -j ${os.cpus().length}`)
+);
+
+var resultName = "image-to-image-of-characters";
+
+if(process.platform == 'win32') {
+    resultName = resultName + ".exe";
+}
+
+fs.copyFileSync(path.join(__dirname, "build", resultName), path.join(__dirname, resultName));
